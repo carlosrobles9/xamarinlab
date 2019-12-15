ebac30b1295136794bd06b1e4212c677576cdb55
carlosrobles9 <carlos_2012@hotmail.es>
2019-12-15 23:21:13 +0100

    practica dec

---
+++ Navegacion/Navegacion/Navegacion/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Navegacion
+{
+    public class Book : ContentPage
+    {
+        internal string titulo;
+        internal string imagen;
+
+        public Book()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello Page" }
+                }
+            };
+        }
+
+        public string Autor { get; internal set; }
+        public string Titulo { get; set; }
+        public string Imagen { get; set; }
+    }
+}
---
+++ Navegacion/Navegacion/Navegacion/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Navegacion
+{
+    public partial class MainPage : ContentPage
+    {
+        List<Book> escritores;
+        public MainPage()
+        {
+            InitializeComponent();
+            escritores = new List<Book>();
+            escritores.Add(new Book()
+            {
+                titulo = "50 Shades Of Grey"
+                ,
+                Autor = "E L  James",
+
+            });
+            escritores.Add(new Book()
+            {
+                titulo = "Bad vibes"
+                ,
+                Autor = "xxxTentation",
+
+            });
+            escritores.Add(new Book()
+            {
+                titulo = "50 Años de mala suerte"
+                ,
+                Autor = "Tom Acosta",
+
+            });
+            escritores.Add(new Book()
+            {
+                titulo = "Love and Music"
+                ,
+                Autor = "Chris Pack"
+                ,
+                
+
+            });
+        }
+    }
+}
+
